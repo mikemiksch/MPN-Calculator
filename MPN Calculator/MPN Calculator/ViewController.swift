548798110a14630e2acabe6f0a795bc4810f664b
mikemiksch <mikemiksch@gmail.com>
2017-05-24 13:54:00 -0700

    Created XCode project and app icon

---
+++ MPN Calculator/MPN Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MPN Calculator
+//
+//  Created by Mike Miksch on 5/24/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
